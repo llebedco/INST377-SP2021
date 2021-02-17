63a5ff94ec4f797acfd3045e64183a1bfb84abd1
llebedco <78024797+llebedco@users.noreply.github.com>
2021-02-17 10:29:44 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,28 +1,19 @@
 /* Put your javascript in here */
-const width = 130; // image width
-const count = 3; // visible images count
-const list = carouselID.querySelector('ul');
-const listElems = carouselID.querySelectorAll('li');
-const position = 0;
 
-carouselID.querySelector('.left').onclick = function() {
-  // shift left
-  position += width * count;
-  // can't move to the left too much, end of images
-  position = Math.min(position, 0);
-  list.style.marginLeft = position + 'px';
-};
-
-carouselID.querySelector('.right').onclick = function() {
-  // shift right
-  position -= width * count;
-  // can only shift the ribbbon for (total ribbon length - visible count) images
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.marginLeft = position + 'px';
-};
+function clickToMove() {
+  document.addEventListener('click', (event) => {
+    const arr = document.list;
+    const classes = arr.classList;
+    console.log(classes.value);
+    const newClass = (classes.value === 'arrow') ? 'left' : 'arrow';
+    console.log(newClass);
+    box.classList = newClass;
+  });
+}
+window.onclick = clickToMove;
 
 function arrayMethod() {
-  const images = document.querySelectorAll('.carouselID');
+  const images = document.querySelectorAll('.arrow');
   const imageArray = Array.from(images);
   console.log(images); 
 
@@ -31,3 +22,5 @@ function arrayMethod() {
 });
 }
 arrayMethod();
+
+

12c0384c4040c4d14bb75097c3def2d886752473
llebedco <78024797+llebedco@users.noreply.github.com>
2021-02-19 14:56:05 -0500

    Create script.js

---
+++ public/labs/lab_4/script.js
@@ -0,0 +1,14 @@
+function onLoad() {
+  console.log('script loaded');
+}
+
+window.onLoad = onLoad;
+
+app.route('/api')
+  .post(async(req, res) => {
+    console.log('POST request detected');
+    console.log('From data in res.body', req.body);
+    res.json('Hello Word');
+  });
+
+  

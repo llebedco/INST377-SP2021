e54ef056cf16a8ac475825ddddda6f779352f57e
llebedco <78024797+llebedco@users.noreply.github.com>
2021-04-01 17:55:44 -0400

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -4,9 +4,7 @@ function clickToMove() {
   document.addEventListener('click', (event) => {
     const arr = document.list;
     const classes = arr.classList;
-    console.log(classes.value);
     const newClass = (classes.value === 'arrow') ? 'left' : 'arrow';
-    console.log(newClass);
     box.classList = newClass;
   });
 }
@@ -15,12 +13,7 @@ window.onclick = clickToMove;
 function arrayMethod() {
   const images = document.querySelectorAll('.arrow');
   const imageArray = Array.from(images);
-  console.log(images); 
-
   imageArray.forEach((images) => {
-     console.log(images);
 });
 }
 arrayMethod();
-
-
